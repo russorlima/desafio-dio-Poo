8723a0cf0ab07a07c666ca501a76c1853fe02caa
Russor <107722401+russorlima@users.noreply.github.com>
2023-03-14 21:00:54 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("Descrição do curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso JavaScript");
+        curso2.setDescricao("descrição curso JavaScript");
+        curso2.setCargaHoraria(6);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria Java");
+        mentoria.setDescricao("descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+    }
 
 }
